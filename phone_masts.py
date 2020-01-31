b0d5da90cf993938b7ca2e4717a037f75c5166dc
Stewart Perrygrove <sperrygrove@gmail.com>
2020-01-31 20:28:21 +0000

    Define dataclass to represent a phone mast and verify structure

--- phone_masts.py
+++ phone_masts.py
@@ -4,6 +4,27 @@ import csv
 import os.path
 import argparse
 
+from dataclasses import dataclass
+from datetime import datetime
+from decimal import Decimal
+
+@dataclass
+class PhoneMast:
+    """
+    Dataclass to represent Phone Mast data
+    """
+    prop_name: str
+    prop_addr1: str
+    prop_addr2: str
+    prop_addr3: str
+    prop_addr4: str
+    unit_name: str
+    tenant: str
+    lease_start_date: date
+    lease_end_date: date
+    lease_years: int
+    current_rent: Decimal
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # Perhaps not necessary since we're dealing with a specific data set
@@ -15,7 +36,18 @@ if __name__ == '__main__':
     if not os.path.exists(fpath):
         raise SystemExit(f'File not found: {fpath}')
 
+    masts = []
     with open(fpath) as csvfile:
         rows = csv.reader(csvfile)
+        next(rows) # omit the header
         for row in rows:
-            print(row)
+            masts.append(
+                PhoneMast(prop_name=row[0], prop_addr1=row[1],
+                    prop_addr2=row[2], prop_addr3=row[3], prop_addr4=row[4],
+                    unit_name=row[5], tenant=row[6],
+                    lease_start_date=datetime.strptime(row[7], '%d %b %Y'),
+                    lease_end_date=datetime.strptime(row[8], '%d %b %Y'),
+                    lease_years=int(row[9]), current_rent=Decimal(row[10]))
+                )
+    for mast in masts:
+        print(mast)
